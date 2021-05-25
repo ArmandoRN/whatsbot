dc4b76d079ae3ff50a959ff49874fac28a3c4632
ArmandoRN <64842834+ArmandoRN@users.noreply.github.com>
2021-05-24 20:57:57 -0500

    Add files via upload

--- src/help.js
+++ src/help.js
@@ -1,25 +1,9 @@
 const help = (prefix) => {
 	return `
-「 *ShanBot by shanduy* 」
-
-◪ *INFORMACION*
-  ❏ Comando: 「  ${prefix}  」
-  ❏ Creador: Shanduy
-  ❏ Como instalar el bot: https://www.youtube.com/watch?v=2LQSzEbpJ-M
-  ❏ Instagram: https://www.instagram.com/thepavos
+「 *ArmandBot* 」
 
 ◪ *PARA USAR EL BOT*
   ❏ Registrate con el comando ${prefix}daftar y tu nombre
-
-◪ *NUEVA ACTUALIZACION*
-  │
-  ├─ ❏ ${prefix}welcome 1
-  └─ La foto del usuario debe estar publica
-
-◪ *RESUELVE TUS DUDAS*
-  │
-  ├─ ❏ ${prefix}creador
-  └─ Dudas o problemas aqui
 
 ◪ *CREAR STICKERS*
   │
@@ -52,37 +36,6 @@ const help = (prefix) => {
   ├─❏ ${prefix}qrcode
   └─ Coloca un texto
 
-◪ *GRUPOS*
-  │
-  ├─ ❏ ${prefix}closecg
-  ├─ Cerrar el grupo solo admins
-  ├─ ❏ ${prefix}opencg
-  ├─ Abrir grupo solo admins
-  ├─ ❏ ${prefix}kick o pafuera
-  ├─ Eliminar a un miembro 
-  ├─ ❏ ${prefix}promote
-  ├─ Dar admin a un miembro
-  ├─ ❏ ${prefix}demote
-  ├─ Quitar el admin
-  ├─ ❏ ${prefix}linkgc
-  ├─ Link del grupo
-  ├─ ❏ ${prefix}tagall
-  └─ Nombra a todos los del grupo
-
-Para usar estas funciones el bot necesita admin
-  
-◪ *NSWF NUEVAS FUNCIONES* 
-  │
-  └─ ❏ ${prefix}nsfwmenu
-
-Para activar los NSFW coloque el siguiente comando ${prefix}nsfw 1 y para desactivar los NSFW coloque ${prefix}nsfw 0
-
-No te olvides de seguirme en instagram flaco :)
-
-Para saber la ultima version mas actual del bot entra en 
-https://github.com/shanduy/ShanBot
-
-by shanduy
 `
 }
 
